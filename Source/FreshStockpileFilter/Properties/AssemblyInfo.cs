b6603f2f3d98f54c55a344f4e2be9182656ae3c2
Poroh <meta.poroh.kun@gmail.com>
2017-01-20 00:49:30 +0300

    assemblyinfo

--- Source/FreshStockpileFilter/Properties/AssemblyInfo.cs
+++ Source/FreshStockpileFilter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FreshStockpileFilter")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Porohkun 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
