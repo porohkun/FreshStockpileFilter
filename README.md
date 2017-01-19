9715222f91d62a2702191075c948ce9eae924693
Poroh <meta.poroh.kun@gmail.com>
2017-01-19 23:55:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fresh stockpile filter mod for RimWorld
+
+targetVersion 0.16.1393
+
+Mod adds additional stockpile filter for fresh items
---
+++ Source/FreshStockpileFilter/SpecialThingFilterWorker_Fresh.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimWorld
+{
+    public class SpecialThingFilterWorker_Fresh : SpecialThingFilterWorker
+    {
+        public override bool CanEverMatch(ThingDef def)
+        {
+            var compProperties = def.GetCompProperties<CompProperties_Rottable>();
+            return ((compProperties != null) && !compProperties.rotDestroys);
+        }
+
+        public override bool Matches(Thing t)
+        {
+            var comps = t as ThingWithComps;
+            if (comps == null) return false;
+            var comp = comps.GetComp<CompRottable>();
+            return (((comp != null) && !((CompProperties_Rottable)comp.props).rotDestroys) && (comp.Stage == RotStage.Fresh));
+        }
+    }
+}
